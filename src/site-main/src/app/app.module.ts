50276fdf1293e7b2c0638289f3a9236a81a5f7bc
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-11-05 13:16:57 -0600

    setup site
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

---
+++ src/site-main/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ClarityModule } from '@clr/angular';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule, RouterModule.forRoot([])],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
