fa0ad3d04f0b5fdaa7dd2e2fcd896f61507f0b63
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-11-06 09:05:56 -0600

    home page
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/site-main/src/app/app.module.ts
+++ src/site-main/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ClarityModule } from '@clr/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import '@clr/icons';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     ClarityModule,
-    RouterModule.forRoot([]),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/site-main/src/environments/environment.prod.ts
+++ src/site-main/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  current: {
+    version: '0.13.8',
+    major: '0.13',
+  },
+  next: {
+    version: '1.0.0-beta.2',
+    major: '1.0',
+  },
 };
