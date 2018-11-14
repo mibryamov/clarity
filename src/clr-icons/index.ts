dd1d63c43e18e00169e958012f8b92d9ac9ec4d5
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-11-14 16:24:55 -0600

    wip - step 1 replace icons with a basic package that is built correctly for tree shaking
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/clr-icons/index.ts
+++ src/clr-icons/index.ts
@@ -3,25 +3,6 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { ClarityIconsApi } from './clr-icons-api';
-import { ClarityIconElement } from './clr-icons-element';
 
-const clarityIcons: ClarityIconsApi = ClarityIconsApi.instance;
-
-interface Window {
-  ClarityIcons: any;
-}
-declare var window: Window;
-
-// check if there is a global object called "ClarityIcons"
-if (typeof window !== 'undefined') {
-  if (!window.hasOwnProperty('ClarityIcons')) {
-    // Setting a global object called "ClarityIcons" to expose the ClarityIconsApi.
-    window.ClarityIcons = clarityIcons;
-  }
-
-  // Defining clr-icon custom element
-  customElements.define('clr-icon', ClarityIconElement);
-}
-
-export { clarityIcons as ClarityIcons };
+export const Airplane = 'airplane';
+export const Truck = 'truck';
--- src/clr-icons/public_api.ts
+++ src/clr-icons/public_api.ts
@@ -3,10 +3,4 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { TextEditShapes } from './text-edit';
-
-export * from './text-edit';
-
-if (typeof window !== 'undefined' && window.hasOwnProperty('ClarityIcons')) {
-  window.ClarityIcons.add(TextEditShapes);
-}
+export * from './index';
