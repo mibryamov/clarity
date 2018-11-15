40441a4c11810d094b6b8fa779cd017bf5e31686
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-11-15 11:10:33 -0600

    wip - step 4 get umd bundles with webpack
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

---
+++ src/clr-icons/shapes/technology.umd.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import {
+  ClrShapeCertificateSVG,
+  ClrShapeHelixSVG,
+  ClrShapeHostSVG,
+  ClrShapeMobileSVG,
+  ClrShapeNoWifiSVG,
+  ClrShapePhoneHandsetSVG,
+  ClrShapeRulerPencilSVG,
+  ClrShapeTerminalSVG,
+  TechnologyShapes,
+} from './technology';
+import { descriptorConfig } from '../utils/descriptor-config';
+
+export * from './technology';
+
+Object.defineProperty(TechnologyShapes, 'server', descriptorConfig(ClrShapeHostSVG));
+Object.defineProperty(TechnologyShapes, 'command', descriptorConfig(ClrShapeTerminalSVG));
+Object.defineProperty(TechnologyShapes, 'mobile-phone', descriptorConfig(ClrShapeMobileSVG));
+Object.defineProperty(TechnologyShapes, 'license', descriptorConfig(ClrShapeCertificateSVG));
+Object.defineProperty(TechnologyShapes, 'disconnected', descriptorConfig(ClrShapeNoWifiSVG));
+Object.defineProperty(TechnologyShapes, 'receiver', descriptorConfig(ClrShapePhoneHandsetSVG));
+Object.defineProperty(TechnologyShapes, 'design', descriptorConfig(ClrShapeRulerPencilSVG));
+Object.defineProperty(TechnologyShapes, 'dna', descriptorConfig(ClrShapeHelixSVG));
+
+if (typeof window !== 'undefined' && window.hasOwnProperty('ClarityIcons')) {
+  window.ClarityIcons.add(TechnologyShapes);
+}
---
+++ src/clr-icons/shapes/travel.umd.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { ClrShapeAirplaneSVG, ClrShapeCarSVG, TravelShapes } from './travel';
+import { descriptorConfig } from '../utils/descriptor-config';
+
+export * from './travel';
+
+Object.defineProperty(TravelShapes, 'plane', descriptorConfig(ClrShapeAirplaneSVG));
+Object.defineProperty(TravelShapes, 'auto', descriptorConfig(ClrShapeCarSVG));
+
+if (typeof window !== 'undefined' && window.hasOwnProperty('ClarityIcons')) {
+  window.ClarityIcons.add(TravelShapes);
+}
